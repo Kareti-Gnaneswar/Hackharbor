6ec5e9c887c8bdd5bfe119d020544f3e6058675c
kareti-gnaneswar <gnaneswarkareti@gmail.com>
2024-08-16 09:38:40 +0530

    first

--- myworld/Scripts/activate.bat
+++ myworld/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\vikaski\hackharbor\myworld
+set VIRTUAL_ENV=E:\hackharbor main\HackHarbor\myworld
 
 if not defined PROMPT set PROMPT=$P$G
 
